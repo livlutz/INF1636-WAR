71ac65fc181e4fae53e9d4cedb2a2015b399d856
Lívia Lutz <108961867+livlutz@users.noreply.github.com>
2023-10-06 23:21:51 -0300

    Create Continente.java

---
+++ src/Model/Continente.java
@@ -0,0 +1,19 @@
+package Model;
+
+import java.util.ArrayList;
+
+class Continente {
+	private String nome;
+	private int qtdExercitoConquista;
+	
+	private ArrayList <Territorio> territorios = new ArrayList <Territorio>();
+	
+	public Continente(String nome,int execConquista) {
+		this.nome = nome;
+		qtdExercitoConquista = execConquista;
+		//colocar todos os territorios que um certo continente possui
+		
+	}
+	
+	//falta o metodo de se e inteiramente dominado por algm
+}
