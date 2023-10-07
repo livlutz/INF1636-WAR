accd708fe3721cb223322dd91490aa12c03cf130
Lívia Lutz <108961867+livlutz@users.noreply.github.com>
2023-10-06 22:49:01 -0300

    Classe do Objetivo

--- src/Model/Objetivo.java
+++ src/Model/Objetivo.java
@@ -1,5 +1,23 @@
 package Model;
 
-public class Objetivo {
+import java.util.ArrayList;
+import java.util.Random;
 
+public class Objetivo {
+  private ArrayList <String> descricao = new ArrayList <String>();
+	
+	public Objetivo() {
+		descricao.add("Dominar a Ásia");
+		descricao.add("Dominar a África");
+		descricao.add("Dominar a Oceania");
+		descricao.add("Dominar a América do Sul");
+		descricao.add("Dominar a América do Norte");
+		descricao.add("Dominar a Europa");
+	}
+	
+	public String getObjetivo() {
+		Random r = new Random();
+		return descricao.get(r.nextInt(descricao.size()));
+	}
+	
 }
