a5d8fd0da2f5b03d1775635ef694e096993cebfc
Lívia Lutz <108961867+livlutz@users.noreply.github.com>
2023-10-06 23:17:23 -0300

    Classe Jogador

--- src/Model/Jogador.java
+++ src/Model/Jogador.java
@@ -1,17 +1,53 @@
 package Model;
 
-public class Jogador {
+import java.util.ArrayList;
+import java.util.Random;
 
-		String cor;
-		Objetivo objetivo;
-		Cartas cartas[];
-		int exercitos;
-		String nome;
-		int qntTrocas;
-		
-		public Jogador(String cor,String nome) {
-			this.cor = cor;
-			this.nome = nome;
-			
+class Jogador {
+
+	// enum com as cores que um jogador pode escolher
+	public enum Cor {
+		branco,preto,vermelho,azul,amarelo,verde;
+	}
+	
+	private Cor cor;
+	private String nome;
+	private int numExercitoPosic;
+	private static int qtdTrocaCartas = 0;
+	private ArrayList <Cartas> cartas = new ArrayList <Cartas> ();
+	Objetivo o = new Objetivo();
+	
+	public void setCor() {
+		Random random = new Random();
+		cor = Cor.values()[random.nextInt(Cor.values().length)];
+	}
+	
+	public Jogador(String nome) {
+		this.nome = nome;
+		setCor();
+	}
+	
+	public void VerObjetivo() {
+		o.exibeObjetivo();
+	}
+
+	public void verCartas() {
+		for(Carta c : cartas) {
+			System.out.println(c);
 		}
+	}
+
+	public int getQtdExecPos() {
+		return numExercitoPosic;
+	}
+
+	public static void trocarCartas (Cartas a, Cartas b, Cartas c) {
+		qtdTrocaCartas++;
+	}
+
+	/*Faltando: 
+ 		Public Getters e setters
+   		Adiconar cartas no array
+     		Territorios e ver qtd de territorios
+ 		*/
 }
