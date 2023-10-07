6c88a4ed65986f8de7b242605e258b7fbb0b3959
Lívia Lutz <108961867+livlutz@users.noreply.github.com>
2023-10-06 23:04:15 -0300

    Update Cartas.java

--- src/Model/Cartas.java
+++ src/Model/Cartas.java
@@ -10,7 +10,7 @@ public class Cartas {
 	
 	public Cartas() {
 		Random r = new Random ();
-		f = Formato.values() [r.nextInt(Forma.values().length)];
+		f = Formato.values() [r.nextInt(Formato.values().length)];
 	}
 
 }
