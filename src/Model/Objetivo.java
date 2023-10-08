ec65c2ecd34526c7141f355cce74718d7eeb9ea3
Lívia Lutz <108961867+livlutz@users.noreply.github.com>
2023-10-08 19:58:07 -0300

    Objetivos definidos

--- src/Model/Objetivo.java
+++ src/Model/Objetivo.java
@@ -11,12 +11,20 @@ class Objetivo {
 		
 	}
 	public static void criaObjetivos() {
-		descricao.add("Dominar a Ásia");
-		descricao.add("Dominar a África");
-		descricao.add("Dominar a Oceania");
-		descricao.add("Dominar a América do Sul");
-		descricao.add("Dominar a América do Norte");
-		descricao.add("Dominar a Europa");
+		descricao.add("Conquistar na totalidade a EUROPA, a OCEANIA e mais um terceiro");
+		descricao.add("Conquistar na totalidade a ÁSIA e a AMÉRICA DO SUL");
+		descricao.add("Conquistar na totalidade a EUROPA, a AMÉRICA DO SUL e mais um terceiro");
+		descricao.add("Conquistar 18 TERRITÓRIOS e ocupar cada um deles com pelo menos dois exércitos");
+		descricao.add("Conquistar na totalidade a ÁSIA e a ÁFRICA");
+		descricao.add("Conquistar na totalidade a AMÉRICA DO NORTE e a ÁFRICA");
+		descricao.add("Conquistar 24 TERRITÓRIOS à sua escolha");
+		descricao.add("Conquistar na totalidade a AMÉRICA DO NORTE e a OCEANIA");
+		descricao.add("Destruir totalmente OS EXÉRCITOS AZUIS");
+		descricao.add("Destruir totalmente OS EXÉRCITOS AMARELOS");
+		descricao.add("Destruir totalmente OS EXÉRCITOS VERMELHOS");
+		descricao.add("Destruir totalmente OS EXÉRCITOS PRETOS");
+		descricao.add("Destruir totalmente OS EXÉRCITOS BRANCOS");
+		descricao.add("Destruir totalmente OS EXÉRCITOS VERDES");
 	}
 	public String getObjetivoAleatorio() {
 		Collections.shuffle(descricao, new Random());
