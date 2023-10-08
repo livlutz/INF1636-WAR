b464a06301d2aeb6ea87bb4136976dd48c09cd13
lumpleme <108637505+lumpleme@users.noreply.github.com>
2023-10-08 11:39:07 -0300

    Territorio com métodos

--- src/Model/Territorio.java
+++ src/Model/Territorio.java
@@ -6,6 +6,56 @@ class Territorio {
 		Jogador jogador;
 		// Posição posicao;
 		int qntExercitos;
-		ArrayList<Territorio> adjacentes;
-		protected Territorio(){}
+		private ArrayList<Territorio> adjacentes = new ArrayList<Territorio>();
+		
+		protected Territorio(){
+			// inicializa para se chamar getters não ter problema
+			this.jogador = NULL;
+			this.qntExercitos = 0;
+		}
+			
+		// Adicionar território adjacente para inicialização
+		protected void AddAdjacente (Territorio adj) {
+			this.adjacentes.add(adj);
+		}
+		
+		protected boolean verificaAdjacencia (Territorio ter) {
+			for (Territorio t: adjacentes)
+				if (ter == t)
+					return true;
+			return false;
+		}
+		
+		// Alterar quantidade de exércitos sem precisar saber o valor antes
+		// Int positivo add exercitos e negativo subtrai 
+		protected boolean alterarQtdExercitos (int qtd) {
+			// se tentar subtrair mais exércitos do que tem (sem poder zerar)
+			if (qtd < 0)
+				if ((qtd * (-1)) >= this.qntExercitos)
+					return false;
+			this.qntExercitos += qtd;
+		}
+		
+		// getters e setters padrão
+		public Jogador getJogador() {
+			return jogador;
+		}
+
+		public void setJogador(Jogador jogador) {
+			this.jogador = jogador;
+		}
+
+		public int getQntExercitos() {
+			return qntExercitos;
+		}
+
+		public void setQntExercitos(int qntExercitos) {
+			this.qntExercitos = qntExercitos;
+		}
+
+		public ArrayList<Territorio> getAdjacentes() {
+			return adjacentes;
+		}
+
+	
 }
