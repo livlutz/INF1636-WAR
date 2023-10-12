4f6f5769a040550eb18bb47c30c582a75e365674
lumpleme <108637505+lumpleme@users.noreply.github.com>
2023-10-12 16:13:03 -0300

    Metodos continente
    (Não escrevi no Eclipse então não tenho certeza da sintaxe)

--- src/Model/Continente.java
+++ src/Model/Continente.java
@@ -9,23 +9,40 @@ class Continente {
 	//Guarda a quantidade de exércitos obtidos pela sua conquista
 	private int qtdExercitoConquista; 
 	
-	//Guarda os territórios presentes no continente
-	protected ArrayList <Territorio> territorios = new ArrayList <Territorio>();
+//Guarda os territórios presentes no continente
+	private ArrayList <Territorio> territorios = new ArrayList <Territorio>();
 	
 	//Construtor
 	public Continente(String nome,int execConquista) {
 		this.nome = nome;
 		qtdExercitoConquista = execConquista;
 	}
-	
-	//verifica se o continente é inteiramente dominado por 1 jogador
-	public boolean continenteDominado(Jogador j) {
-		int contaMesmoJogador = 0;
-		for(Territorio t:territorios) {
-			if(t.getJogador() == j) {
-				contaMesmoJogador++;
-			}
+
+	public ArrayList <Territorio> getTerritorios(){
+		return this.territorios;
+	}
+
+	public String getNome(){
+		return this.nome;
+	}
+
+	public int getQtdExerc(){
+		return this.qtdExercitoConquista;
+	}
+
+	// Adicionar Territorios no continente
+	public void addTerritorio(Territorio ter){
+		this.territorios.add(ter);
+	}
+
+	// Verifica se o continente está inteiramente dominado por algum jogador
+	public Jogador dominado(){
+		Jogador jogador = territorios[0].getJogador();
+		for (Territorio t: territorios){
+			if (t.getJogador() != jogador)
+				return NULL;
 		}
-		return contaMesmoJogador == territorios.size();
+		return jogador;
 	}
 }
+ 
