1b03e25ea96ef62799dbd63e95dc7f36f5da00dd
Livia <livialutz385@gmail.com>
2023-10-14 03:16:04 -0300

    Teste objetivo com OBS
    Falta terminar a implementação em objetivo

--- Teste/Model/TesteObjetivo.java
+++ Teste/Model/TesteObjetivo.java
@@ -5,10 +5,20 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class TesteObjetivo {
-
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testeConstrutor() {
+		Objetivo o = new Objetivo();
+		assertNotNull(o);
+	}
+	
+	//tem q ver se e assim q testa 
+	@Test
+	public void testeGetObjAleatorio() {
+		Objetivo o = new Objetivo();
+		Objetivo.criaObjetivos();
+		String s = Objetivo.descricao.get(0);
+		assertEquals(o.getObjetivoAleatorio(),s);
 	}
 
 }
