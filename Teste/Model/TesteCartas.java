8f2260403ff36ade3239907384a48f87dfb2a2e7
Livia <livialutz385@gmail.com>
2023-10-27 02:24:18 -0300

    Testando métodos em cartas

--- Teste/Model/TesteCartas.java
+++ Teste/Model/TesteCartas.java
@@ -12,5 +12,17 @@ public class TesteCartas {
 		Cartas c = new Cartas (1,t);
 		assertNotNull(c);
 	}
+	
+	@Test
+	public void testaTerrPertenceJogador() {
+		Territorio t = new Territorio("Teste");
+		Cartas c = new Cartas (1,t);
+		Jogador j = new Jogador(null, 0);
+		Jogador a = new Jogador(null, 1);
+		j.addTerritorio(t);
+		assertTrue(c.terrPertenceJogador(j));
+		assertFalse(c.terrPertenceJogador(a));
+	}
+	
 
 }
