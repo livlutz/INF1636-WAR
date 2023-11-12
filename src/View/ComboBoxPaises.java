ead328cc06807950d84e372a4f98935a0aeaaa30
Livia <livialutz385@gmail.com>
2023-11-12 19:17:54 -0300

    Array de string de territorio local

--- src/View/ComboBoxPaises.java
+++ src/View/ComboBoxPaises.java
@@ -6,14 +6,15 @@ import Model.APIJogo;
 
 public class ComboBoxPaises extends JComboBox<String>{
 	APIJogo	apiJogo = APIJogo.getAPIJogo();
-	String[] territorios = apiJogo.getTerritoriosLista();
 	
 	public ComboBoxPaises(){
 		adicionaPaises();
 	}
 	
 	private void adicionaPaises() {
+		String[] territorios = apiJogo.getTerritoriosLista();
 		for (String t: territorios) {
+			System.out.printf("%s\n",t);
 			this.addItem(t);
 		}
 		
