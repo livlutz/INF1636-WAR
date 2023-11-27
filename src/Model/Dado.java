321d049c60e5e2035b90bc62188840fe575373aa
Livia <livialutz385@gmail.com>
2023-11-27 19:47:43 -0300

    Dados rodando aleatoriamente sem scanner

--- src/Model/Dado.java
+++ src/Model/Dado.java
@@ -9,22 +9,11 @@ class Dado {
 	
 	//Roda o dado que retorna o número de sua face
 	public int rodarDado() {
-		//Pegar um inteiro correspondente a face do dado
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Deseja escolher a face do dado? (1 - Sim, 0 - Não)");
-		String escolha = sc.nextLine();
-		if(escolha == "1"){
-			System.out.println("Digite um número de 1 a 6 correspondente a face do dado:");
-			int face = sc.nextInt();
-			sc.close();
-			return face;
-		}
 		
-		else{
-			Random rand = new Random();
-			sc.close();
-			return rand.nextInt(6);
-		}
+		//Roda dado aleatoriamente
+		Random rand = new Random();
+		return rand.nextInt(6);
+	
 			
 	}
 }
